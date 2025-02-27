8c87687a0a8b33ca05e14d96914ff8b71c45f3da
Yogesh Negi <yogeshlm13@gmail.com>
2025-02-27 22:37:57 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -80,7 +80,7 @@ app.get("/referrals", async (req, res) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 'https://refernearn.onrender.com';
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 
